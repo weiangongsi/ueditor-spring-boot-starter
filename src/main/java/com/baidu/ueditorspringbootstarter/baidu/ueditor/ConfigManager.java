479ef6d61e2ba8785c838db7b96a6ad96526f482
lihaoyang <lihaoyang@huayugroup360.com>
2018-09-26 15:16:43 +0800

    解决打包后不能读取配置文件问题

--- src/main/java/com/baidu/ueditorspringbootstarter/baidu/ueditor/ConfigManager.java
+++ src/main/java/com/baidu/ueditorspringbootstarter/baidu/ueditor/ConfigManager.java
@@ -16,10 +16,7 @@ import java.util.Map;
 public final class ConfigManager {
 
     private final String rootPath;
-    private final String originalPath;
-    private final String contextPath;
     private static final String configFileName = "config.json";
-    private String parentPath = null;
     private JSONObject jsonConfig = null;
     // 涂鸦上传filename定义
     private final static String SCRAWL_FILE_NAME = "scrawl";
@@ -30,20 +27,9 @@ public final class ConfigManager {
      * 通过一个给定的路径构建一个配置管理器， 该管理器要求地址路径所在目录下必须存在config.properties文件
      */
     private ConfigManager(String rootPath, String contextPath, String uri) throws FileNotFoundException, IOException {
-
         rootPath = rootPath.replace("\\", "/");
-
         this.rootPath = rootPath;
-        this.contextPath = contextPath;
-
-        if (contextPath.length() > 0) {
-            this.originalPath = this.rootPath + uri.substring(contextPath.length());
-        } else {
-            this.originalPath = this.rootPath + uri;
-        }
-
         this.initEnv();
-
     }
 
     /**
@@ -70,18 +56,14 @@ public final class ConfigManager {
     }
 
     public JSONObject getAllConfig() {
-
         return this.jsonConfig;
-
     }
 
     public Map<String, Object> getConfig(int type) {
 
         Map<String, Object> conf = new HashMap<String, Object>();
         String savePath = null;
-
         switch (type) {
-
             case ActionMap.UPLOAD_FILE:
                 conf.put("isBase64", "false");
                 conf.put("maxSize", this.jsonConfig.getLong("fileMaxSize"));
@@ -89,7 +71,6 @@ public final class ConfigManager {
                 conf.put("fieldName", this.jsonConfig.getString("fileFieldName"));
                 savePath = this.jsonConfig.getString("filePathFormat");
                 break;
-
             case ActionMap.UPLOAD_IMAGE:
                 conf.put("isBase64", "false");
                 conf.put("maxSize", this.jsonConfig.getLong("imageMaxSize"));
@@ -97,14 +78,12 @@ public final class ConfigManager {
                 conf.put("fieldName", this.jsonConfig.getString("imageFieldName"));
                 savePath = this.jsonConfig.getString("imagePathFormat");
                 break;
-
             case ActionMap.UPLOAD_VIDEO:
                 conf.put("maxSize", this.jsonConfig.getLong("videoMaxSize"));
                 conf.put("allowFiles", this.getArray("videoAllowFiles"));
                 conf.put("fieldName", this.jsonConfig.getString("videoFieldName"));
                 savePath = this.jsonConfig.getString("videoPathFormat");
                 break;
-
             case ActionMap.UPLOAD_SCRAWL:
                 conf.put("filename", ConfigManager.SCRAWL_FILE_NAME);
                 conf.put("maxSize", this.jsonConfig.getLong("scrawlMaxSize"));
@@ -135,82 +114,48 @@ public final class ConfigManager {
                 break;
 
         }
-
         conf.put("savePath", savePath);
         conf.put("rootPath", this.rootPath);
-
         return conf;
-
     }
-
-    private void initEnv() throws FileNotFoundException, IOException {
-
-        File file = new File(this.originalPath);
-
-        if (!file.isAbsolute()) {
-            file = new File(file.getAbsolutePath());
-        }
-
-        this.parentPath = file.getParent();
-
-        String configContent = this.readFile(this.getConfigPath());
-
+    private void initEnv() throws IOException {
+        String configContent = this.readFile(this.rootPath + "/" + ConfigManager.configFileName);
         try {
             JSONObject jsonConfig = new JSONObject(configContent);
             this.jsonConfig = jsonConfig;
         } catch (Exception e) {
             this.jsonConfig = null;
         }
-
-    }
-
-    private String getConfigPath() {
-        return this.parentPath + File.separator + ConfigManager.configFileName;
     }
 
     private String[] getArray(String key) {
-
         JSONArray jsonArray = this.jsonConfig.getJSONArray(key);
         String[] result = new String[jsonArray.length()];
-
         for (int i = 0, len = jsonArray.length(); i < len; i++) {
             result[i] = jsonArray.getString(i);
         }
-
         return result;
-
     }
 
     private String readFile(String path) throws IOException {
-
         StringBuilder builder = new StringBuilder();
-
         try {
-
-            InputStreamReader reader = new InputStreamReader(new FileInputStream(path), "UTF-8");
+            InputStreamReader reader = new InputStreamReader(this.getClass().getClassLoader().getResourceAsStream(path), "UTF-8");
             BufferedReader bfReader = new BufferedReader(reader);
-
             String tmpContent = null;
-
             while ((tmpContent = bfReader.readLine()) != null) {
                 builder.append(tmpContent);
             }
-
             bfReader.close();
-
         } catch (UnsupportedEncodingException e) {
             // 忽略
         }
-
         return this.filter(builder.toString());
-
     }
 
     // 过滤输入字符串, 剔除多行注释以及替换掉反斜杠
     private String filter(String input) {
-
         return input.replaceAll("/\\*[\\s\\S]*?\\*/", "");
-
     }
 
 }
